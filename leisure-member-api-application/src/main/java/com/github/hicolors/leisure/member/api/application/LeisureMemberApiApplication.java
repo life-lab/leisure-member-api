5808c5a89019347ba991e2c2f0b55ef3b2716050
李伟超 <liweichao0102@gmail.com>
2018-11-15 10:15:44 +0800

    初识版本提交

---
+++ leisure-member-api-application/src/main/java/com/github/hicolors/leisure/member/api/application/LeisureMemberApiApplication.java
@@ -0,0 +1,19 @@
+package com.github.hicolors.leisure.member.api.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * LeisureMemberApiApplication
+ *
+ * @author weichao.li (liweichao0102@gmail.com)
+ * @date 2018/11/14
+ */
+@SpringBootApplication
+public class LeisureMemberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeisureMemberApiApplication.class, args);
+    }
+
+}
